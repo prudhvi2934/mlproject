c65deeb9ddcb0dc2f897bf919bc594510573629d
Prudhvi Sajja <spk2934@gmail.com>
2023-05-16 12:16:26 +0100

    Prediction pipeline

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import numpy as np
 import pandas as pd
 from src.exception import CustomException
 import dill
+import pickle
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
 
@@ -36,7 +37,7 @@ def evaluate_model(X_train,y_train,X_test,y_test,models,param):
 
             model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
-            
+
             #model.fit(X_train,y_train)  # Train model
 
             y_train_pred = model.predict(X_train)
@@ -50,6 +51,16 @@ def evaluate_model(X_train,y_train,X_test,y_test,models,param):
             report[list(models.keys())[i]] = test_model_score
         
         return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
